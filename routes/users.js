06ee885f28337dabb417c470a23e9337bbeeebff
emanuel caro <darkemlord@gmail.com>
2022-12-10 15:29:44 +0900

    refs 001 improving syntax in newUser action

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/user.model");
+const jwt = require("jsonwebtoken");
+require("dotenv").config();
 
 /* GET users listing. */
 router.get("/", function (req, res) {
@@ -10,13 +12,17 @@ router.get("/", function (req, res) {
 router.post("/", (req, res) => {
   const userData = req.body;
   const newUser = new User(userData);
+  const { name, email } = newUser;
+  const token = jwt.sign({ name, email }, process.env.SECRET, {
+    expiresIn: "24h",
+  });
   newUser.save((err, user) => {
     if (err && err.code === 11000)
       return res.json({
         error: `User ${req.body.email} already exist`,
       });
     if (err) return res.json({ error: err });
-    res.json({ message: `User ${user.name} Saved Succesfully` });
+    res.json({ message: `User ${user.name} Saved Succesfully`, token });
   });
 });
 
